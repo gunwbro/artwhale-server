a6d8dc24acf07b0ad24eeddec31598cd65f0bccf
unknown <gunwbro@gmail.com>
2022-10-16 21:19:05 +0900

    swagger setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ArtWhale API 문서')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+  console.log('Server listening on port ' + (process.env.PORT || 3000));
 }
 bootstrap();
